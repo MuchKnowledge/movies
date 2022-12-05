b13fd70798691d6f3aee6c60a4dce7c6881d6ca2
nicola.gordanovich <muchknowledgetho@gmail.com>
2022-12-05 18:16:26 +0400

    @UPDATE() clean unnecessary db layer

--- app/src/main/java/com/example/app/data/repositories/MoviesRepositoryImpl.kt
+++ app/src/main/java/com/example/app/data/repositories/MoviesRepositoryImpl.kt
@@ -1,8 +1,6 @@
 package com.example.app.data.repositories
 
 import com.example.app.data.api.MoviesApi
-import com.example.app.data.db.dao.MoviesDao
-import com.example.app.data.db.entities.movie.MovieEntity
 import com.example.app.data.mappers.responseToData
 import com.example.app.data.models.MovieData
 import com.example.app.data.models.ResponseResult
@@ -12,7 +10,6 @@ import javax.inject.Inject
 
 class MoviesRepositoryImpl @Inject constructor(
     private val moviesApi: MoviesApi,
-    private val moviesDao: MoviesDao,
 ) : MoviesRepository {
 
     override suspend fun searchMoviesByTitle(title: String, page: Int): ResponseResult<List<MovieData>> {
@@ -27,10 +24,5 @@ class MoviesRepositoryImpl @Inject constructor(
 
     override suspend fun loadMovieById(title: String): MovieData {
         return moviesApi.loadMovieById(title).responseToData()
-    }
-
-    override suspend fun updateMovieLikeStatus(id: String, isLiked: Boolean) {
-        val entity = MovieEntity(id, isLiked)
-        moviesDao.saveMovie(entity)
     }
 }
